42943da594af3bc40a4b6fdcd1d8659aed0962b3
Rahulpuram <77501084+Rahulpuram@users.noreply.github.com>
2023-12-14 15:13:39 +0530

    Update APIRoutes.js

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,6 +1,7 @@
 export const host = "http://localhost:5000";
-export const loginRoute = `${host}/api/auth/login`;
-export const registerRoute = `${host}/api/auth/register`;
+
+export const loginRoute = process.env.REACT_APP_BASE_URL+`/api/auth/login`;
+export const registerRoute = process.env.REACT_APP_BASE_URL`/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute = `${host}/api/messages/addmsg`;
